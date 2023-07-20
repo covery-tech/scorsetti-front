f2fbe631a9f88da0af0e661c4885238e83ccd13e
sansito84 <santiagosito@gmail.com>
2023-07-19 22:29:14 -0300

    fix products

--- src/components/SectionProducts/ProductCard.jsx
+++ src/components/SectionProducts/ProductCard.jsx
@@ -1,6 +1,5 @@
 import './ProductCard.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCar, faMotorcycle, faHome, faShip, faUserInjured, faHeart, faBuilding, faStore, faUmbrellaBeach, faHandHoldingUsd, faHandshake, faHeartbeat, faUserMd, faPeopleCarry, faPaw, faRestroom, faMoneyBillAlt, faAngellist, faShoppingCart, faAddressBook, faHourglassEnd, faUserPlus, faIdCard } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProductCard({ props }) {
 
@@ -8,7 +7,7 @@ export default function ProductCard({ props }) {
         <div className="item align-content-center p-3"  >
             <div className="service" data-toggle="tooltip" data-placement="top" title={props?.title2}>
                 <div className="icon">
-                    <FontAwesomeIcon icon={props?.img} />
+                    <FontAwesomeIcon className='img' icon={props?.img} />
                     {/* <img className='img' src={props?.img}  alt={props?.title}/> */}
                 </div>
                 <h4>{props?.title}</h4>
