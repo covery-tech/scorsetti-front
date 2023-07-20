576b5253676ca556eba696e381f6b05b628ec02a
Mansilla <marcos.rehtanz@gmail.com>
2023-07-20 18:44:45 -0300

    filter products in SectionProducts

--- src/components/SectionProducts/SectionProducts.jsx
+++ src/components/SectionProducts/SectionProducts.jsx
@@ -18,7 +18,7 @@ export default function SectionProducts({ userId }) {
         if (res.data) {
           setUserPas(res.data);
         }
-      }).catch((err)=>{
+      }).catch((err) => {
         console.error(err);
       });
     } else {
@@ -28,35 +28,54 @@ export default function SectionProducts({ userId }) {
       };
       axios(config2).then((res) => {
         setUserPas(res.data);
-      }).catch((err)=>{
+      }).catch((err) => {
         console.error(err);
       });
     }
   }, [userId]);
+  // SWITCH
+  const [clientSection, setClientSection] = useState('Persona');
+  const changeClient = () => {
+    (clientSection === 'Persona')
+      ? setClientSection('Empresa')
+      : setClientSection('Persona');
+  }
+  // PUBLICACIÓN DE CARD PRODUCT
+  const returnCardProduct = (p, i) => {
+    return (
+      <Link key={i} to={`/producto/${p}`} className="aProducts">
+        <ProductCard props={infoProducts[p]} />
+      </Link>
+    );
+  }
 
   return (
     <div className="container bg-white mt-5 p-5">
       <PrincipalText />
+      <button onClick={changeClient}>{clientSection}</button>
       <div className="row justify-content-around">
-        {userId ? (
-          <div className="content">
-            {userPas.products?.map((p) => (
-              <Link to={'producto/' + p}>
-                <ProductCard props={infoProducts[p]} />
-              </Link>
-            ))}
-          </div>
-        ) : (
-          <div className="content">
-            {listProducts.map((p, i) => {
-              return (
-                <Link key={i} to={`/producto/${p}`} className="aProducts">
-                  <ProductCard props={infoProducts[p]} />
+        {
+          userId ? (
+            <div className="content">
+              {userPas.products?.map((p) => (
+                <Link to={'producto/' + p}>
+                  if(true){
+                    <ProductCard props={infoProducts[p]} />
+                  }
                 </Link>
-              );
-            })}
-          </div>
-        )}
+              ))}
+            </div>
+          ) : (
+            <div className="content">
+              {listProducts.map((p, i) => {
+                if (clientSection==='Persona' && i<18) {
+                  return returnCardProduct(p, i);
+                } else if(clientSection==='Empresa' && i>=18){
+                  return returnCardProduct(p, i);
+                }
+              })}
+            </div>
+          )}
       </div>
     </div>
   );
