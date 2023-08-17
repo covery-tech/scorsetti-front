18473830d9c057aa42ac12024652078036bf4b83
patriciopg02 <patriciopg02_@hotmail.com>
2023-08-17 20:59:43 -0300

    navbar to tachyons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,84 +6,79 @@ import useUser from "../hooks/UseUser";
 
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const {pas} = useUser()
+  const { pas } = useUser();
   const handleNavToggle = () => {
     setIsNavOpen(!isNavOpen);
   };
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <div>
-          {
-            !pas ? 
-            <Link  to="/" className="navbar-brand">
-              <img src="/logo-scorsetti.png" alt="#" />
-              {/* logo mobile */}
+    <nav className="navbar justify-between bg-white flex pv2 ph2 mb5">
+      <div className="w-30-l w-10 logo-desktop">
+        {!pas ? (
+          <Link to="/">
+            <img src="/logo-scorsetti.png" alt="#" />
+            {/* logo mobile */}
+          </Link>
+        ) : (
+          <Link to={`/usuario_pas/${pas.id}`}>
+            <img src="/logo-scorsetti.png" alt="#" />
+            {/* logo mobile */}
+          </Link>
+        )}
+      </div>
+      <div className="w-30-l w-10 logo-mobile">
+        {!pas ? (
+          <Link to="/">
+            <img src="/logo-scorsetti-mobile.png" alt="#" />
+            {/* logo mobile */}
+          </Link>
+        ) : (
+          <Link to={`/usuario_pas/${pas.id}`}>
+            <img src="/logo-scorsetti-mobile.png" alt="#" />
+            {/* logo mobile */}
+          </Link>
+        )}
+      </div>
+      <div className="flex w-70-l w-90 justify-around items-center">
+        <span className="nav-item">
+          {!pas ? (
+            <Link to="/" className="nav-link active">
+              Inicio
             </Link>
-            :
-            <Link  to={`/usuario_pas/${pas.id}`} className="navbar-brand">
-              <img src="/logo-scorsetti.png" alt="#" />
-              {/* logo mobile */}
+          ) : (
+            <Link to={`/usuario_pas/${pas.id}`} className="nav-link active">
+              Inicio
             </Link>
-          }
-        </div>
-        <div className="d-flex">
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={handleNavToggle}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <span className="mobile-item">
-            <UserMenu />
-          </span>
-        </div>
-        <div
-          className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}
-        >
-          <ul className="navbar-nav ms-auto mb-lg-0 p-2">
-            <li className="nav-item">
-            {
-              !pas ?
-              <Link to="/" className="nav-link active">
-                Inicio
-              </Link>
-            : <Link to={`/usuario_pas/${pas.id}`}className="nav-link active">
-                Inicio
-              </Link>
-            } 
-            </li>
-            <li className="nav-item">
-            {
-              !pas ?
-              <a href="/#quienes-somos" className="nav-link">
-                ¿Quiénes somos?
-              </a>
-              :
-              <a href={`/usuario_pas/${pas.id}/#quienes-somos`} className="nav-link">
-                ¿Quiénes somos?
-              </a>
-            }
-            </li>
-            <li className="nav-item">
-            {
-              !pas ?
-              <a href="/#contacto" className="nav-link">
-                Contacto y Ubicación
-              </a>
-              :
-              <a href={`/usuario_pas/${pas.id}/#contacto`} className="nav-link">
+          )}
+        </span>
+        <span className="nav-item">
+          {!pas ? (
+            <a href="/#quienes-somos" className="nav-link">
+              ¿Quiénes somos?
+            </a>
+          ) : (
+            <a
+              href={`/usuario_pas/${pas.id}/#quienes-somos`}
+              className="nav-link"
+            >
+              ¿Quiénes somos?
+            </a>
+          )}
+        </span>
+        <span className="nav-item">
+          {!pas ? (
+            <a href="/#contacto" className="nav-link">
               Contacto y Ubicación
-              </a>
-            }
-            </li>
-            <li className="nav-item desk-item">
-              <UserMenu />
-            </li>
-          </ul>
-        </div>
+            </a>
+          ) : (
+            <a href={`/usuario_pas/${pas.id}/#contacto`} className="nav-link">
+              Contacto y Ubicación
+            </a>
+          )}
+        </span>
+        <span className="nav-item desk-item">
+          <UserMenu />
+        </span>
       </div>
     </nav>
   );
