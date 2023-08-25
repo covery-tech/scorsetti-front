df4df98041e550a8f85108b7333b36d02efaab1a
sansito84 <santiagosito@gmail.com>
2023-08-24 21:20:36 -0300

    fix:TableNotis delete noti

--- src/components/DashBoard/TableNotis/TableNotisAdmin.jsx
+++ src/components/DashBoard/TableNotis/TableNotisAdmin.jsx
@@ -1,115 +1,138 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import useUser from '../../hooks/UseUser'
-import { toast } from 'react-toastify'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useUser from "../../hooks/UseUser";
+import { toast } from "react-toastify";
 
-export const TableNotisAdmin = ({notis,setPage,loading,showShowMore,loadingNextPage,setLoading,setLoadingNextPage,setNotis,page,setShowShowMore}) => {
-    const {jwt} = useUser() 
+export const TableNotisAdmin = ({
+    notis,
+    setPage,
+    loading,
+    showShowMore,
+    loadingNextPage,
+    setLoading,
+    setLoadingNextPage,
+    setNotis,
+    page,
+    setShowShowMore,
+    deleteNoti
+}) => {
+    const { jwt } = useUser();
     //(notis)
-    const getNotis = ()=>{
+    const getNotis = () => {
         const config = {
-            method:"get",
+            method: "get",
             baseURL: `${process.env.REACT_APP_URI_API}/product/getNotificationAdmin/${page}`,
-            headers:{token:jwt}
-        }
-        axios(config).then(e=>{
-            if(e.data.length === 0 || e.data.length<6) setShowShowMore(false)
-            setLoading(false)
-            setLoadingNextPage(false)
-            setNotis(notis.concat(e.data))
-        }).catch(e=>{
-        setLoadingNextPage(false)
-            setLoading(false)
-            alert("error de servidor: Not Found")
-        })
-    }
+            headers: { token: jwt },
+        };
+        axios(config)
+            .then((e) => {
+                if (e.data.length === 0 || e.data.length < 6)
+                    setShowShowMore(false);
+                setLoading(false);
+                setLoadingNextPage(false);
+                setNotis(notis.concat(e.data));
+            })
+            .catch((e) => {
+                setLoadingNextPage(false);
+                setLoading(false);
+                alert("error de servidor: Not Found");
+            });
+    };
 
-
-    useEffect(()=>{
-        if(!notis?.length) {
-            setLoading(true)
+    useEffect(() => {
+        if (!notis?.length) {
+            setLoading(true);
         }
-        setLoadingNextPage(true)
-        getNotis()
-    },[page])
+        setLoadingNextPage(true);
+        getNotis();
+    }, [page]);
 
-    const deleteNoti = (id)=>{
-        const config = {
-            method:"put",
-            baseURL: `${process.env.REACT_APP_URI_API}/product/deleteNotificationAdmin/${id}`,
-            headers:{token:jwt}
-        }
-        axios(config).then(e=>{
-            if(e.data){
-                toast.success("Notificación eliminada", {
-                    position: "bottom-center",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                  });                   
-                let info = notis.flat()
-                let contador = 0 
-                info.map(noti=>{
-                    if(noti.id==id){
-                        //(noti.id)
-                        info.splice(contador,1)
-                    }
-                    contador++
-                })
-                //(info)
-                setNotis(info)
-            }else alert("server error")
-        }).catch(e=>{
-            //(e)
-        })
-    }
-  return ( 
-    <div className="container bg-light rounded-3 m-5 justify-content-center text-center mx-auto">
-        {
-        loading 
-            ? 
-            <div>Cargando...</div>
-            : 
-            <table className="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col"></th>
-                <th scope="col">Nombre y Apellido</th>
-                <th scope="col">Telefono</th>
-                <th scope="col">Email</th>
-                <th scope="col">Descripción</th>
-                <th scope="col">Visitar Perfil</th>
-                <th scope="col">Eliminar Notificación</th>
-            </tr>
-            </thead>
-            {notis?.length ? (
-            <tbody>
-                {notis.map((e) => (
-                    <tr key={e.id}>
-                    <td><img src={`${process.env.REACT_APP_URI_API}/${e?.img}`} style={{height:40,borderRadius:40}}/></td>
-                    <td>{e?.name},{e?.last_name}</td>
-                    <td>{e?.phone_number}</td>
-                    <td>{e?.email}</td>
-                    <td>{e?.description}</td>
-                    <td><Link to={`user/${e?.idPas}`} style={{color:"#0d6efd",cursor:"pointer"}} >Ver Usuario</Link></td>
-                    <td><strong style={{color:"#dc3545",cursor:"pointer"}} onClick={()=>deleteNoti(e?.id)} >Eliminar</strong></td>
-                    </tr>
-                ))}
-            </tbody>
+    return (
+        <div className="container bg-light rounded-3 m-5 justify-content-center text-center mx-auto">
+            {loading ? (
+                <div>Cargando...</div>
             ) : (
-            <></>
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col"></th>
+                            <th scope="col">Nombre y Apellido</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Descripción</th>
+                            <th scope="col">Visitar Perfil</th>
+                            <th scope="col">Eliminar Notificación</th>
+                        </tr>
+                    </thead>
+                    {notis?.length ? (
+                        <tbody>
+                            {notis.map((e) => (
+                                <tr key={e.id}>
+                                    <td>
+                                        <img
+                                            src={`${process.env.REACT_APP_URI_API}/${e?.img}`}
+                                            style={{
+                                                height: 40,
+                                                borderRadius: 40,
+                                            }}
+                                        />
+                                    </td>
+                                    <td>
+                                        {e?.name},{e?.last_name}
+                                    </td>
+                                    <td>{e?.phone_number}</td>
+                                    <td>{e?.email}</td>
+                                    <td>{e?.description}</td>
+                                    <td>
+                                        <Link
+                                            to={`user/${e?.idPas}`}
+                                            style={{
+                                                color: "#0d6efd",
+                                                cursor: "pointer",
+                                            }}
+                                        >
+                                            Ver Usuario
+                                        </Link>
+                                    </td>
+                                    <td>
+                                        <strong
+                                            style={{
+                                                color: "#dc3545",
+                                                cursor: "pointer",
+                                            }}
+                                            onClick={() =>
+                                                deleteNoti(
+                                                    e?.id,
+                                                    "notification_for_user"
+                                                )
+                                            }
+                                        >
+                                            Eliminar
+                                        </strong>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    ) : (
+                        <></>
+                    )}
+                </table>
             )}
-            </table>
-        }
-        {
-            showShowMore ? loadingNextPage ?<strong>cargando...</strong>:<strong onClick={()=>setPage(prev=>prev+1)} style={{color:"#0d6efd",cursor:"pointer"}}>mostrar mas resultados</strong>:<strong>Ops parece que a llegado al final</strong>
-        }
-        
+            {showShowMore ? (
+                loadingNextPage ? (
+                    <strong>cargando...</strong>
+                ) : (
+                    <strong
+                        onClick={() => setPage((prev) => prev + 1)}
+                        style={{ color: "#0d6efd", cursor: "pointer" }}
+                    >
+                        mostrar mas resultados
+                    </strong>
+                )
+            ) : (
+                <strong>Ops parece que a llegado al final</strong>
+            )}
         </div>
-  )
-}
+    );
+};
